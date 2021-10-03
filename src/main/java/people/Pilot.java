8313e3037d7ff567eddcc6332492852a1b743983
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-03 13:06:02 +0100

    Pilot class and methods wriiten, tests passing'

--- src/main/java/people/Pilot.java
+++ src/main/java/people/Pilot.java
@@ -1,4 +1,20 @@
 package people;
 
-public class Pilot {
+public class Pilot extends Crew{
+
+    private String licenceNo;
+
+    public Pilot(String name, Rank rank, String licenceNo) {
+        super(name, rank);
+        this.licenceNo = licenceNo;
+    }
+
+    public String getLicenceNo(){
+        return this.licenceNo;
+    }
+
+    public void setLicenceNo(){
+        this.licenceNo = licenceNo;
+    }
+
 }
--- src/test/java/peopleTests/PilotTest.java
+++ src/test/java/peopleTests/PilotTest.java
@@ -1,4 +1,33 @@
 package peopleTests;
 
+import org.junit.Before;
+import org.junit.Test;
+import people.Pilot;
+import people.Rank;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    private Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Clarence Oveur", Rank.CAPTAIN, "A01-ROGER");
+    }
+
+    @Test
+    public void testPilotHasName(){
+        assertEquals("Clarence Oveur", pilot.getName());
+    }
+
+    @Test
+    public void testPilotHasRank(){
+        assertEquals(Rank.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void testPilotHasLicenceNo(){
+        assertEquals("A01-ROGER", pilot.getLicenceNo());
+    }
 }
