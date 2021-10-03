3099629af3b91f55281347468b29d1e30d9132ee
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-03 13:31:49 +0100

    Passenger class and methods created, tests written and passing

--- src/main/java/people/Passenger.java
+++ src/main/java/people/Passenger.java
@@ -1,4 +1,19 @@
 package people;
 
-public class Passenger {
+public class Passenger extends Person{
+
+    private int baggage;
+
+    public Passenger(String name, int baggage){
+        super(name);
+        this.baggage = baggage;
+    }
+
+    public int getBaggage() {
+        return baggage;
+    }
+
+    public void setBaggage(int baggage) {
+        this.baggage = baggage;
+    }
 }
--- src/test/java/peopleTests/PassengerTest.java
+++ src/test/java/peopleTests/PassengerTest.java
@@ -1,4 +1,27 @@
 package peopleTests;
 
+import org.junit.Before;
+import org.junit.Test;
+import people.Passenger;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Ted Striker", 20);
+    }
+
+    @Test
+    public void checkPassengerHasName(){
+        assertEquals("Ted Striker", passenger.getName());
+    }
+
+    @Test
+    public void checkPassengerHasBaggage(){
+        assertEquals(20, passenger.getBaggage());
+    }
 }
