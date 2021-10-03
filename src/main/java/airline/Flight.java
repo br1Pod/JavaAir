c8e389582d39020e072fcb9c9d42ded7a43a472f
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-03 22:26:50 +0100

    flight class written - test pilotHasName passed

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -1,4 +1,100 @@
 package airline;
 
+import people.Crew;
+import people.Passenger;
+import people.Pilot;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Pilot> pilots;
+    private ArrayList<Crew> crew;
+    private ArrayList<Passenger> passengers;
+    private PlaneType planeType;
+    private String flightNumber;
+    private String destination;
+    private String departurePort;
+    private String departureTime;
+
+    public Flight(
+            PlaneType planeType,
+            String flightNumber,
+            String destination,
+            String departurePort,
+            String departureTime
+    ) {
+        this.pilots = new ArrayList<>();
+        this.crew = new ArrayList<>();
+        this.passengers = new ArrayList<>();
+        this.planeType = planeType;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departurePort = departurePort;
+        this.departureTime = departureTime;
+    }
+
+    public ArrayList<Pilot> getPilots() {
+        return pilots;
+    }
+
+    public void setPilots(ArrayList<Pilot> pilots) {
+        this.pilots = pilots;
+    }
+
+    public ArrayList<Crew> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(ArrayList<Crew> crew) {
+        this.crew = crew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public PlaneType getPlaneType() {
+        return planeType;
+    }
+
+    public void setPlaneType(PlaneType planeType) {
+        this.planeType = planeType;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDeparturePort() {
+        return departurePort;
+    }
+
+    public void setDeparturePort(String departurePort) {
+        this.departurePort = departurePort;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 }
--- src/test/java/airlineTests/FlightTest.java
+++ src/test/java/airlineTests/FlightTest.java
@@ -1,4 +1,35 @@
 package airlineTests;
 
+import airline.Flight;
+import airline.Plane;
+import airline.PlaneType;
+import org.junit.Before;
+import org.junit.Test;
+import people.Crew;
+import people.Passenger;
+import people.Pilot;
+import people.Rank;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    private Crew pilot;
+    private Crew steward;
+    private Passenger passenger_1;
+    private Passenger passenger_2;
+    private Flight flight;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Clarence Oveur", Rank.CAPTAIN, "A01-ROGER");
+        steward = new Crew("Elaine Dickinson", Rank.STEWARD);
+        passenger_1 = new Passenger("Ted Striker", 20);
+        passenger_2 = new Passenger("Dr Rumack", 2);
+        flight = new Flight(PlaneType.BOEING747, "FL01", "EDI", "GLA", "10.00");
+    }
+
+    @Test
+    public void pilotHasName(){
+        assertEquals("Clarence Oveur", pilot.getName());
+    }
 }
